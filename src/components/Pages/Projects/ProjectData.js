d262a7194346566adf688a518a6f5b8b6db6dd5f
Larry LaMar <larrylamar@Larrys-iMac.lan>
2022-01-18 23:42:42 -0600

    added project cards and images

---
+++ src/components/Pages/Projects/ProjectData.js
@@ -0,0 +1,52 @@
+import LoftRentalImage from "../../../images/loft rental img.png";
+import CarGalleryImage from "../../../images/car gallery.png";
+import LaunchChecklistImage from "../../../images/launch checklist.png";
+import TechJobsImage from "../../../images/techjobs.png";
+import OrbitReportImage from "../../../images/orbit report.png";
+
+export const ProjectData = [
+  {
+    link: "https://l2fc3s.github.io/react-loftRental-website/#/",
+    title: "React 'Loft Rental' Site",
+    image: LoftRentalImage,
+    text: 'React "loft rental" landing page. Fully mobile responsive design with an animated image slider gallery.',
+    alt: "React 'Loft Rental' Site",
+    repo: "https://github.com/l2fc3s/react-loftRental-website",
+  },
+
+  {
+    link: "https://l2fc3s.github.io/html-me-something/#body",
+    title: "Car Photo Gallery",
+    image: CarGalleryImage,
+    text: "A small gallery of car photos I have taken over the last few years. Each photo displays a short paragraph on mouse hover. Responsive design for mobile and desktop with collapsible navigation bar.",
+    alt: "Car Photo Gallery",
+    repo: "github repo link",
+  },
+
+  {
+    link: " https://l2fc3s.github.io/Launch-Checklist-Form/ ",
+    title: "Launch Checklist Form",
+    image: LaunchChecklistImage,
+    text: "An html/css form with javascript validators for entry and data type. Uses API call to randomly fetch planetary data on load.",
+    alt: "Launch Checklist Form",
+    repo: "https://github.com/l2fc3s/Launch-Checklist-Form",
+  },
+
+  {
+    link: " https://github.com/l2fc3s/java-web-dev-techjobs-persistent",
+    title: "Java TechJobs",
+    image: TechJobsImage,
+    text: "This is a project done in LaunchCode LC101.  I contributed by connecting the application to a MySQL database. I also Implemented features that allow the user to add a new job, employer, and skills to the web application and database. The user can also search for specific jobs, employers, or skills. Search results show in a will list, or the user can view jobs by category.",
+    alt: "Java TechJobs",
+    repo: "https://github.com/l2fc3s/java-web-dev-techjobs-persistent",
+  },
+
+  {
+    link: " https://github.com/l2fc3s/orbit-report",
+    title: "Orbit Report",
+    image: OrbitReportImage,
+    text: " This is a project built using Angular framework. It features a table of Satellite names including data for each. The table is sortable by column, and filters based on search results. It also shows total number of satellites in the table, as well as the count for each type of satellite.",
+    alt: "Orbit Report",
+    repo: "https://github.com/l2fc3s/orbit-report",
+  },
+];
--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ProjectData } from "./ProjectData";
+import "./Projects.css";
 
 function Projects() {
   return (
-    <div>
-      <p>This is a damn test</p>
+    <div className="project-section">
+      <div className="project-container">
+        {ProjectData.map((project) => {
+          return (
+            <div className="project-card">
+              <Link to={project.link}>
+                <img
+                  className="project-image"
+                  src={project.image}
+                  alt={project.alt}
+                />
+              </Link>
+              <h2 className="project-title">{project.title}</h2>
+              <p className="description">{project.text}</p>
+              <Link className="view-more" to={project.repo}>
+                View on Github
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
